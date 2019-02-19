5c36258755ee58ddbff044754e43e0904d26e3ac
jcock <jason.cockerham@gmail.com>
2019-02-19 15:47:03 -0500

    testing out HTTP/2 push for meta icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,6 +92,23 @@ module.exports = {
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-sitemap`,
 		`gatsby-plugin-netlify-cache`,
-		`gatsby-plugin-netlify` // Must be last in the plugins array
+		// `gatsby-plugin-netlify` // Must be last in the plugins array
+		{
+			resolve: `gatsby-plugin-netlify`,
+			options: {
+				allPageHeaders: [
+					'Link: </apple-touch-icon.png>; rel=preload; as=image',
+					'Link: </android-chrome-192x192.png>; rel=preload; as=image',
+					'Link: </android-chrome-256x256.png>; rel=preload; as=image',
+					'Link: </android-chrome-512x512.png>; rel=preload; as=image',
+					'Link: </favicon-16x16.png>; rel=preload; as=image',
+					'Link: </favicon-32x32.png>; rel=preload; as=image',
+					'Link: </mstile-70x70.png>; rel=preload; as=image',
+					'Link: </mstile-150x150.png>; rel=preload; as=image',
+					'Link: </mstile-310x150.png>; rel=preload; as=image',
+					'Link: </manifest.webmanifest>; rel=preload; as=webmanifest'
+				]
+			}
+		}
 	]
 };
