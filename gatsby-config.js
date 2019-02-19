0366ed04684cd4759031f5ab4e824e20b3c07423
jcock <jason.cockerham@gmail.com>
2019-02-19 14:24:17 -0500

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,6 @@ module.exports = {
 				name: `assets`
 			}
 		},
-		// {
-		// 	resolve: `gatsby-source-filesystem`,
-		// 	options: {
-		// 		path: `${__dirname}/content/posts`,
-		// 		name: `posts`
-		// 	}
-		// },
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
