2f911bff66a067bd91ddd085038967780b8ff9fe
jcock <jason.cockerham@gmail.com>
2019-02-20 09:12:26 -0500

    Updated ssr with preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,7 @@ const React = require('react');
 exports.onRenderBody = ({ setHeadComponents }) => {
 	setHeadComponents([
 		<link rel="dns-prefetch" key="dns-prefetch-google-analytics" href="https://www.google-analytics.com" />,
-		<link rel="dns-prefetch" key="dns-prefetch-google" href="https://www.google.com" />,
-		<link rel="dns-prefetch" key="dns-prefetch-google-marketingplatform" href="https://marketingplatform.google.com" />
+		<link rel="preconnect" key="preconnect-google" href="https://www.google.com" />,
+		<link rel="preconnect" key="preconnect-google-marketingplatform" href="https://marketingplatform.google.com" />
 	]);
 };
