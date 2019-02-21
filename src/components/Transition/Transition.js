beb5d64034afb8f9da3bd5535c419455d4849e61
jcock <jason.cockerham@gmail.com>
2019-02-21 16:34:22 -0500

    Updated page transitions

--- src/components/Transition/Transition.js
+++ src/components/Transition/Transition.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import posed, { PoseGroup } from 'react-pose';
 
-const timeout = 0;
+import style from './Transition.module.css';
+
+const timeout = 300;
+const duration = 600;
 
 class Transition extends React.PureComponent {
 	render() {
 		const { children, location } = this.props;
 
 		const RoutesContainer = posed.div({
-			entering: {
-				opacity: 0
-			},
 			enter: {
-				opacity: 1,
-				filter: `blur(0px)`,
-				y: 0,
 				delay: timeout,
-				delayChildren: timeout
-			},
-			exiting: {
-				y: 0
+				delayChildren: timeout,
+				filter: `blur(0px)`,
+				opacity: 1,
+				y: 0,
+				transition: {
+					y: { type: 'spring', stiffness: 1000, damping: 15 },
+					duration
+				}
 			},
 			exit: {
-				opacity: 0,
 				filter: `blur(20px)`,
-				y: 30
+				opacity: 0,
+				y: 10,
+				transition: { duration: `${duration / 2}` }
 			}
 		});
 
 		return (
 			<PoseGroup>
-				<RoutesContainer key={location.pathname}>{children}</RoutesContainer>
+				<RoutesContainer className={style.transitionContainer} key={location.pathname}>
+					{children}
+				</RoutesContainer>
 			</PoseGroup>
 		);
 	}
