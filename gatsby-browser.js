9efa452004599a62756969fefbabebfefc7c3862
jcock <jason.cockerham@gmail.com>
2019-02-21 11:07:53 -0500

    Added Transition component with react-pose

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
-require('prismjs/themes/prism-tomorrow.css');
+import React from 'react';
+import Transition from './src/components/Transition';
+
+import 'prismjs/themes/prism-tomorrow.css';
+
+// Wrap Pages In Transition
+// eslint-disable-next-line import/prefer-default-export
+export const wrapPageElement = ({ element, props }) => {
+	return <Transition {...props}>{element}</Transition>;
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,18 @@
-const React = require('react');
+import React from 'react';
+import Transition from './src/components/Transition';
 
-exports.onRenderBody = ({ setHeadComponents }) => {
+// Set up some <head> components
+// eslint-disable-next-line import/prefer-default-export
+export const onRenderBody = ({ setHeadComponents }) => {
 	setHeadComponents([
 		<link rel="dns-prefetch" key="dns-prefetch-google-analytics" href="https://www.google-analytics.com" />,
 		<link rel="preconnect" key="preconnect-google" href="https://www.google.com" />,
 		<link rel="preconnect" key="preconnect-google-marketingplatform" href="https://marketingplatform.google.com" />
 	]);
+};
+
+// Wrap Pages In Transition
+// eslint-disable-next-line import/prefer-default-export
+export const wrapPageElement = ({ element, props }) => {
+	return <Transition {...props}>{element}</Transition>;
 };
