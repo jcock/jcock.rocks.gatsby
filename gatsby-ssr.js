b4c2cacc7264455e0f9ac13cbb4e085ee944c6b1
jcock <jason.cockerham@gmail.com>
2019-02-22 09:20:26 -0500

    Removed commented code from gatsby-ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import Transition from './src/components/Transition';
 
-// Set up some <head> components
-// eslint-disable-next-line import/prefer-default-export
-// export const onRenderBody = ({ setHeadComponents }) => {
-// 	setHeadComponents([
-// 		<link rel="dns-prefetch" key="dns-prefetch-google-analytics" href="https://www.google-analytics.com" />,
-// 		<link rel="preconnect" key="preconnect-google" href="https://www.google.com" />,
-// 		<link rel="preconnect" key="preconnect-google-marketingplatform" href="https://marketingplatform.google.com" />
-// 	]);
-// };
-
 // Wrap Pages In Transition
 // eslint-disable-next-line import/prefer-default-export
 export const wrapPageElement = ({ element, props }) => {
