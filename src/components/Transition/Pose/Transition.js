9d63b7d3d8780fe09549250cc4092a5f4d772c78
jcock <jason.cockerham@gmail.com>
2019-03-11 14:22:55 -0400

    Added prefers-reduced-motion media query to transition

--- src/components/Transition/Pose/Transition.js
+++ src/components/Transition/Pose/Transition.js
@@ -7,28 +7,29 @@ import style from '../Transition.module.css';
 class Transition extends React.PureComponent {
 	render() {
 		const { children, location } = this.props;
+		const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches === true;
 		const scrollTop = () => window.scrollTo(0, 0);
 
 		const config = {
-			delay: 400,
-			duration: 400,
+			durationIn: prefersReducedMotion ? 0 : 400,
+			durationOut: prefersReducedMotion ? 0 : 400,
 			ease: `easeInOut`
 		};
 
 		const RouteContainer = posed.div({
 			enter: {
-				delay: config.delay,
+				delay: config.durationOut,
 				beforeChildren: true,
 				opacity: 1,
 				transition: {
-					duration: config.duration,
+					duration: config.durationIn,
 					ease: config.ease
 				}
 			},
 			exit: {
 				opacity: 0,
 				transition: {
-					duration: config.duration,
+					duration: config.durationOut,
 					ease: config.ease
 				}
 			}
